de240bd0316acf445534383b5775e103cc28b648
Mikhail Mikheev <mmvsha73@gmail.com>
2020-10-13 19:08:33 +0400

    add type for rpc call names

--- src/eth/method.ts
+++ src/eth/method.ts
@@ -1,13 +1,18 @@
 import { sendMessageToInterface, SDK_MESSAGES } from 'src/communication';
-import { SentSDKMessage, RequestId } from 'src/types';
+import { SentSDKMessage, RequestId, RpcCallNames } from 'src/types';
 
-const buildRequest = (call: string) => (...params: unknown[], requestId?: RequestId): SentSDKMessage<'RPC_CALL'> => {
+type RequestArgs<T> = {
+  params: T;
+  requestId?: RequestId;
+};
+
+const buildRequest = (call: RpcCallNames) => (args: RequestArgs): SentSDKMessage<'RPC_CALL'> => {
   const payload = {
     call,
-    params,
+    params: args.params,
   };
 
-  const message = sendMessageToInterface(SDK_MESSAGES.RPC_CALL, payload, requestId);
+  const message = sendMessageToInterface(SDK_MESSAGES.RPC_CALL, payload, args.requestId);
 
   return message;
 };
