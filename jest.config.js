fd972a9102c1ecb41f1647900580d3631a1c6483
Mikhail Mikheev <mmvsha73@gmail.com>
2020-10-14 13:53:27 +0400

    add types for rpc calls

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
 };
--- src/eth/index.ts
+++ src/eth/index.ts
@@ -1,10 +1,11 @@
 import { buildRequest } from './method';
+import { TransactionConfig, PastLogsOptions } from 'web3-core';
 
 const eth = {
-  call: buildRequest('eth_call'),
+  call: buildRequest<TransactionConfig>('eth_call'),
   getBalance: buildRequest<string>('eth_getBalance'),
   getCode: buildRequest<string>('eth_getCode'),
-  getPastLogs: buildRequest('eth_getLogs'),
+  getPastLogs: buildRequest<PastLogsOptions>('eth_getLogs'),
   getBlockByHash: buildRequest<string>('eth_getBlockByHash'),
   getBlockByNumber: buildRequest<number>('eth_getBlockByNumber'),
   getStorageAt: buildRequest<string>('eth_getStorageAt'),
