4c0fe0d422f55b7ab431634bf022add2c329f516
Mikhail Mikheev <mmvsha73@gmail.com>
2020-10-20 17:27:49 +0400

    add input formatters

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -2,20 +2,33 @@ import { TransactionConfig, PastLogsOptions } from 'web3-core';
 import { RPC_CALLS } from '../eth/constants';
 import { buildRequest } from './request';
 
+const inputFormatters = {
+  defaultBlockParam: (...args: unknown[]) => console.log(args),
+};
+
 const eth = {
-  call: buildRequest<TransactionConfig, typeof RPC_CALLS.eth_call>(RPC_CALLS.eth_call),
-  getBalance: buildRequest<string, typeof RPC_CALLS.eth_getBalance>(RPC_CALLS.eth_getBalance),
-  getCode: buildRequest<string, typeof RPC_CALLS.eth_getCode>(RPC_CALLS.eth_getCode),
-  getPastLogs: buildRequest<PastLogsOptions, typeof RPC_CALLS.eth_getLogs>(RPC_CALLS.eth_getLogs),
-  getBlockByHash: buildRequest<string, typeof RPC_CALLS.eth_getBlockByHash>(RPC_CALLS.eth_getBlockByHash),
-  getBlockByNumber: buildRequest<number, typeof RPC_CALLS.eth_getBlockByNumber>(RPC_CALLS.eth_getBlockByNumber),
-  getStorageAt: buildRequest<string, typeof RPC_CALLS.eth_getStorageAt>(RPC_CALLS.eth_getStorageAt),
-  getTransactionByHash: buildRequest<string, typeof RPC_CALLS.eth_getTransactionByHash>(
-    RPC_CALLS.eth_getTransactionByHash,
-  ),
-  getTransactionReceipt: buildRequest<string, typeof RPC_CALLS.eth_getTransactionReceipt>(
-    RPC_CALLS.eth_getTransactionReceipt,
-  ),
+  call: buildRequest<[TransactionConfig, string?], typeof RPC_CALLS.eth_call>({
+    call: RPC_CALLS.eth_call,
+    inputFormatters: [inputFormatters.defaultBlockParam],
+  }),
+  getBalance: buildRequest<[string, string?], typeof RPC_CALLS.eth_getBalance>({ call: RPC_CALLS.eth_getBalance }),
+  getCode: buildRequest<[string, string?], typeof RPC_CALLS.eth_getCode>({ call: RPC_CALLS.eth_getCode }),
+  getPastLogs: buildRequest<PastLogsOptions, typeof RPC_CALLS.eth_getLogs>({ call: RPC_CALLS.eth_getLogs }),
+  getBlockByHash: buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByHash>({
+    call: RPC_CALLS.eth_getBlockByHash,
+  }),
+  getBlockByNumber: buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByNumber>({
+    call: RPC_CALLS.eth_getBlockByNumber,
+  }),
+  getStorageAt: buildRequest<[string, string?], typeof RPC_CALLS.eth_getStorageAt>({
+    call: RPC_CALLS.eth_getStorageAt,
+  }),
+  getTransactionByHash: buildRequest<string, typeof RPC_CALLS.eth_getTransactionByHash>({
+    call: RPC_CALLS.eth_getTransactionByHash,
+  }),
+  getTransactionReceipt: buildRequest<string, typeof RPC_CALLS.eth_getTransactionReceipt>({
+    call: RPC_CALLS.eth_getTransactionReceipt,
+  }),
 };
 
 export { eth };
--- src/eth/request.ts
+++ src/eth/request.ts
@@ -1,12 +1,24 @@
 import { sendMessageToInterface, SDK_MESSAGES } from '../communication';
 import { SentSDKMessage, RequestArgs, RpcCallNames, RPCPayload } from '../types';
 
-const buildRequest = <P extends unknown, C extends RpcCallNames>(call: C) => (
-  args: RequestArgs<P>,
-): SentSDKMessage<'RPC_CALL', RPCPayload<C, P>> => {
+const buildRequest = <P extends unknown, C extends RpcCallNames>({
+  call,
+  inputFormatters,
+}: {
+  call: C;
+  inputFormatters?: ((...args: unknown[]) => unknown)[];
+}) => (args: RequestArgs<P>): SentSDKMessage<'RPC_CALL', RPCPayload<C, P>> => {
+  const params = args.params;
+
+  if (inputFormatters) {
+    inputFormatters.forEach((formatter) => {
+      formatter(args.params);
+    });
+  }
+
   const payload = {
     call,
-    params: args.params,
+    params,
   };
 
   const message = sendMessageToInterface(SDK_MESSAGES.RPC_CALL, payload, args.requestId);
