797cd4f9435196affd793bcecf8cd51e18af9306
Mikhail Mikheev <mmvsha73@gmail.com>
2020-10-27 20:47:08 +0400

    breaking stuff continues 4

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -9,37 +9,69 @@ const inputFormatters = {
 
 class EthMethods {
   constructor(communicator) {
-    this.call = buildRequest<[TransactionConfig, string?], typeof RPC_CALLS.eth_call>({
+    this.call = this.buildRequest<[TransactionConfig, string?], typeof RPC_CALLS.eth_call>({
       call: RPC_CALLS.eth_call,
       inputFormatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getBalance = buildRequest<[string, string?], typeof RPC_CALLS.eth_getBalance>({
+    this.getBalance = this.buildRequest<[string, string?], typeof RPC_CALLS.eth_getBalance>({
       call: RPC_CALLS.eth_getBalance,
       inputFormatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getCode = buildRequest<[string, string?], typeof RPC_CALLS.eth_getCode>({
+    this.getCode = this.buildRequest<[string, string?], typeof RPC_CALLS.eth_getCode>({
       call: RPC_CALLS.eth_getCode,
       inputFormatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getStorageAt = buildRequest<[string, string?], typeof RPC_CALLS.eth_getStorageAt>({
+    this.getStorageAt = this.buildRequest<[string, string?], typeof RPC_CALLS.eth_getStorageAt>({
       call: RPC_CALLS.eth_getStorageAt,
       inputFormatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getPastLogs = buildRequest<[PastLogsOptions], typeof RPC_CALLS.eth_getLogs>({ call: RPC_CALLS.eth_getLogs });
-    this.getBlockByHash = buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByHash>({
+    this.getPastLogs = this.buildRequest<[PastLogsOptions], typeof RPC_CALLS.eth_getLogs>({
+      call: RPC_CALLS.eth_getLogs,
+    });
+    this.getBlockByHash = this.buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByHash>({
       call: RPC_CALLS.eth_getBlockByHash,
       inputFormatters: [null, inputFormatters.fullTxObjectParam],
     });
-    this.getBlockByNumber = buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByNumber>({
+    this.getBlockByNumber = this.buildRequest<[string, boolean?], typeof RPC_CALLS.eth_getBlockByNumber>({
       call: RPC_CALLS.eth_getBlockByNumber,
       inputFormatters: [null, inputFormatters.fullTxObjectParam],
     });
-    this.getTransactionByHash = buildRequest<[string], typeof RPC_CALLS.eth_getTransactionByHash>({
+    this.getTransactionByHash = this.buildRequest<[string], typeof RPC_CALLS.eth_getTransactionByHash>({
       call: RPC_CALLS.eth_getTransactionByHash,
     });
-    this.getTransactionReceipt = buildRequest<[string], typeof RPC_CALLS.eth_getTransactionReceipt>({
+    this.getTransactionReceipt = this.buildRequest<[string], typeof RPC_CALLS.eth_getTransactionReceipt>({
       call: RPC_CALLS.eth_getTransactionReceipt,
     });
+  }
+
+  private buildRequest<P extends unknown | unknown[], C extends RpcCallNames>({
+    call,
+    inputFormatters,
+  }: {
+    call: C;
+    /* eslint-disable-next-line */
+    inputFormatters?: (((arg: any) => any) | null)[];
+  }) {
+    return function (args: RequestArgs<P>): SentSDKMessage<'RPC_CALL', RPCPayload<C, P>> {
+      const params = args.params;
+
+      if (inputFormatters && Array.isArray(params)) {
+        inputFormatters.forEach((formatter: ((...args: unknown[]) => unknown) | null, i) => {
+          if (formatter) {
+            params[i] = formatter((args.params as unknown[])[i]);
+          }
+        });
+      }
+
+      const payload = {
+        call,
+        params,
+      };
+
+      const message = sendMessageToInterface(SDK_MESSAGES.RPC_CALL, payload, args.requestId);
+
+      return message;
+    };
   }
 }
 
