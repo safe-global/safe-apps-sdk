09602db055659383149e25e7b21b357c3cc59b0d
Mikhail Mikheev <mmvsha73@gmail.com>
2020-10-28 17:36:42 +0400

    txs class wip

--- src/txs/index.ts
+++ src/txs/index.ts
@@ -1,8 +1,30 @@
 import { setTxServiceUrl } from './envInfo';
 import { getBySafeTxHash } from './getBySafeTxHash';
 
-const txs = {
-  getBySafeTxHash,
-};
+class TXs {
+  async getBySafeTxHash(safeTxHash: string): Promise<TxServiceModel> {
+    const txServiceUrl = getTxServiceUrl();
 
-export { txs, setTxServiceUrl };
+    if (!txServiceUrl) {
+      throw new Error("ENV information hasn't been synced yet or there was an error during the process");
+    }
+
+    const controller = new AbortController();
+    const options = {
+      method: 'GET',
+      signal: controller.signal,
+    };
+    setTimeout(() => controller.abort(), 10000);
+
+    try {
+      const res = await fetch(`${txServiceUrl}/transactions/${safeTxHash}`, options);
+      const json = await res.json();
+
+      return json as TxServiceModel;
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+export default TXs;
