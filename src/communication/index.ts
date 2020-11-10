4db16c5ea2e877140572dc3f2dfbc87a7056fbce
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-10 14:06:05 +0400

    include sdk version, check method validity

--- src/communication/index.ts
+++ src/communication/index.ts
@@ -1,4 +1,5 @@
 import { InterfaceMessageEvent, Communicator, Methods, MethodToParams, MethodToResponse } from '../types';
+import { METHODS } from './methods';
 import { generateRequestId, DEFAULT_ALLOWED_ORIGINS } from './utils';
 
 // eslint-disable-next-line
@@ -7,9 +8,11 @@ type Callback = (response: any) => void;
 class InterfaceCommunicator implements Communicator {
   private allowedOrigins: RegExp[] = [];
   private callbacks = new Map<string, Callback>();
+  sdkVersion: string;
 
-  constructor(allowedOrigins: RegExp[]) {
+  constructor(allowedOrigins: RegExp[], sdkVersion: string) {
     this.allowedOrigins = [...DEFAULT_ALLOWED_ORIGINS, ...allowedOrigins];
+    this.sdkVersion = sdkVersion;
 
     window.addEventListener('message', this.onParentMessage);
   }
@@ -18,8 +21,9 @@ class InterfaceCommunicator implements Communicator {
     const emptyOrMalformed = !data;
     const unknownOrigin = this.allowedOrigins?.find((regExp) => regExp.test(origin)) === undefined;
     const sameOrigin = origin === window.origin;
+    const knownMethod = Object.values(METHODS).includes(data.method);
 
-    return !emptyOrMalformed && !unknownOrigin && !sameOrigin;
+    return !emptyOrMalformed && !unknownOrigin && !sameOrigin && knownMethod;
   };
 
   private logIncomingMessage = (msg: InterfaceMessageEvent): void => {
@@ -53,6 +57,7 @@ class InterfaceCommunicator implements Communicator {
       method,
       requestId,
       data,
+      env: { sdkVersion: this.sdkVersion },
     };
 
     if (typeof window !== 'undefined') {
