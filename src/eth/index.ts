542d5114ff9265dc779136e9481d7fc97e6ad9d0
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-12 18:06:31 +0400

    [breaks things]: add types to rpc calls

--- src/eth/index.ts
+++ src/eth/index.ts
@@ -1,6 +1,14 @@
 import { TransactionConfig, PastLogsOptions } from 'web3-core';
 import { RPC_CALLS } from '../eth/constants';
-import { RpcCallNames, RequestArgs, Communicator } from './../types';
+import {
+  RpcCallNames,
+  RequestArgs,
+  Communicator,
+  Log,
+  BlockTransactionString,
+  BlockTransactionObject,
+  Web3TransactionObject,
+} from './../types';
 import { METHODS } from '../communication/methods';
 
 const inputFormatters = {
@@ -23,42 +31,42 @@ class Eth {
 
   constructor(communicator: Communicator) {
     this.#communicator = communicator;
-    this.call = this.buildRequest<[TransactionConfig, string?]>({
+    this.call = this.buildRequest<[TransactionConfig, string?], string>({
       call: RPC_CALLS.eth_call,
       formatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getBalance = this.buildRequest<[string, string?]>({
+    this.getBalance = this.buildRequest<[string, string?], string>({
       call: RPC_CALLS.eth_getBalance,
       formatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getCode = this.buildRequest<[string, string?]>({
+    this.getCode = this.buildRequest<[string, string?], string>({
       call: RPC_CALLS.eth_getCode,
       formatters: [null, inputFormatters.defaultBlockParam],
     });
-    this.getStorageAt = this.buildRequest<[string, number, string?]>({
+    this.getStorageAt = this.buildRequest<[string, number, string?], string>({
       call: RPC_CALLS.eth_getStorageAt,
       formatters: [null, inputFormatters.numberToHex, inputFormatters.defaultBlockParam],
     });
-    this.getPastLogs = this.buildRequest<[PastLogsOptions]>({
+    this.getPastLogs = this.buildRequest<[PastLogsOptions], Log[]>({
       call: RPC_CALLS.eth_getLogs,
     });
-    this.getBlockByHash = this.buildRequest<[string, boolean?]>({
+    this.getBlockByHash = this.buildRequest<[string, boolean?], BlockTransactionString | BlockTransactionObject>({
       call: RPC_CALLS.eth_getBlockByHash,
       formatters: [null, inputFormatters.fullTxObjectParam],
     });
-    this.getBlockByNumber = this.buildRequest<[string, boolean?]>({
+    this.getBlockByNumber = this.buildRequest<[string, boolean?], BlockTransactionString | BlockTransactionObject>({
       call: RPC_CALLS.eth_getBlockByNumber,
       formatters: [inputFormatters.numberToHex, inputFormatters.fullTxObjectParam],
     });
-    this.getTransactionByHash = this.buildRequest<[string]>({
+    this.getTransactionByHash = this.buildRequest<[string], Web3TransactionObject>({
       call: RPC_CALLS.eth_getTransactionByHash,
     });
-    this.getTransactionReceipt = this.buildRequest<[string]>({
+    this.getTransactionReceipt = this.buildRequest<[string], Web3TransactionObject>({
       call: RPC_CALLS.eth_getTransactionReceipt,
     });
   }
 
-  private buildRequest<P extends unknown[]>({
+  private buildRequest<P extends unknown[], R = Record<string, string>>({
     call,
     formatters,
   }: {
@@ -66,7 +74,7 @@ class Eth {
     /* eslint-disable-next-line */
     formatters?: (((arg: any) => any) | null)[];
   }) {
-    return async (args: RequestArgs<P>): Promise<Record<string, string>> => {
+    return async (args: RequestArgs<P>): Promise<R> => {
       const params = args.params;
 
       if (formatters && Array.isArray(params)) {
