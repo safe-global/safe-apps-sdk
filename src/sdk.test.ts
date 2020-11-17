10484a3afd58a0a7a6d13504aee5ec60dbe00ebd
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-17 19:44:22 +0400

    add a test that checks getEnvInfo is called on init

--- src/sdk.test.ts
+++ src/sdk.test.ts
@@ -10,8 +10,20 @@ describe('Safe apps SDK', () => {
     });
 
     test('Should initialize without regExp', () => {
-      sdkInstance = new SDK([/http:\/\/localhost:3000/]);
+      sdkInstance = new SDK([]);
       expect(sdkInstance.txs.send).not.toBeUndefined();
+    });
+
+    test("should send a getEnvInfo message to obtain information about interface's env", () => {
+      const spy = jest.spyOn(window.parent, 'postMessage');
+      sdkInstance = new SDK([]);
+
+      expect(spy).toHaveBeenCalledWith(
+        expect.objectContaining({
+          method: 'getEnvInfo',
+        }),
+        '*',
+      );
     });
   });
 });
