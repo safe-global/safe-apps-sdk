e80e84fc12f7b5c9a0da40fae91ce20bd56375f2
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-18 18:45:08 +0400

    Add export for formatter

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export default SDK;
 export * from './sdk';
 export * from './types';
 export * from './communication/methods';
+export * from './communication/messageFormatter';
 export { getSDKVersion } from './utils';
