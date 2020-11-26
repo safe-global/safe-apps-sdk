27f7052da70efe2cbdee8797fa50e58873cc03bc
Mikhail Mikheev <mmvsha73@gmail.com>
2020-11-26 17:34:24 +0400

    jest setup wip

--- packages/safe-apps-sdk/jest.config.js
+++ packages/safe-apps-sdk/jest.config.js
@@ -1,12 +1,6 @@
-'use strict';
-
-const packageName = require('./package.json').name.split('@gnosis.pm/').pop();
-
 module.exports = {
-  roots: [`<rootDir>/packages/${packageName}`],
-  moduleDirectories: ['node_modules'],
-  modulePaths: [`<rootDir>/packages/${packageName}/src/`],
-  name: packageName,
-  displayName: packageName,
-  rootDir: '../..',
+  preset: 'ts-jest',
+  clearMocks: true,
+  moduleDirectories: ['node_modules', 'src'],
+  roots: ['src'],
 };
