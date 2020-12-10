4c0542e7dcf0d50688921cfbf297a4a55bd8f0db
Mikhail Mikheev <mmvsha73@gmail.com>
2020-12-10 13:22:41 +0400

    moar fixes

--- packages/safe-app-cra-template/template/src/GlobalStyle.ts
+++ packages/safe-app-cra-template/template/src/GlobalStyle.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from "styled-components";
-import avertaFont from "@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2";
-import avertaBoldFont from "@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2";
+import { createGlobalStyle } from 'styled-components';
+import avertaFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-normal.woff2';
+import avertaBoldFont from '@gnosis.pm/safe-react-components/dist/fonts/averta-bold.woff2';
 
 const GlobalStyle = createGlobalStyle`
     html {
