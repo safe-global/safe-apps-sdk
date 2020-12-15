9e32202ff82d9ea629db0eecc9efa2dfd13a3289
Mikhail Mikheev <mmvsha73@gmail.com>
2020-12-15 16:04:33 +0400

    fix cra template import

--- packages/cra-template-safe-app/template/src/index.tsx
+++ packages/cra-template-safe-app/template/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '@gnosis.pm/safe-react-components';
 import { Loader, Title } from '@gnosis.pm/safe-react-components';
-import SafeProvider from '@rmeissner/safe-apps-react-sdk';
+import SafeProvider from '@gnosis.pm/safe-apps-react-sdk';
 
 import GlobalStyle from './GlobalStyle';
 import App from './App';
