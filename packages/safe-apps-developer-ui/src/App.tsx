c223968cc34551eecc325123e663714616380cd4
Mikhail Mikheev <mmvsha73@gmail.com>
2021-03-01 15:34:26 +0400

    bootstrapping

--- packages/safe-apps-developer-ui/src/App.tsx
+++ packages/safe-apps-developer-ui/src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
