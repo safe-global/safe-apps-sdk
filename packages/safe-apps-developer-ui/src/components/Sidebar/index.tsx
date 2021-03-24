daf89f42c051d1ba622967cbd06022e73b729ffc
Mikhail Mikheev <mmvsha73@gmail.com>
2021-03-24 18:56:34 +0400

    sidebar wip

--- packages/safe-apps-developer-ui/src/components/Sidebar/index.tsx
+++ packages/safe-apps-developer-ui/src/components/Sidebar/index.tsx
@@ -1,10 +1,16 @@
-import { useParams } from 'react-router-dom';
+import React from 'react';
+import { useParams, Link, LinkProps, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import { ETHEREUM_NETWORK_TO_ID } from 'src/api/provider';
 import { useProviderStore } from 'src/stores/provider';
 import { upperFirst } from 'src/utils/strings';
 import { headerHeight } from 'src/styles/variables';
 import { SafeHeader } from './SafeHeader';
+import ÅppsIcon from './apps.svg';
 
 const SidebarContainer = styled.aside`
   width: 200px;
@@ -23,15 +29,44 @@ const Line = styled.hr`
   border: none;
 `;
 
+interface ListItemLinkProps {
+  icon?: React.ReactElement;
+  primary: string;
+  to: string;
+}
+
+// https://material-ui.com/ru/guides/composition/#list
+function ListItemLink(props: ListItemLinkProps) {
+  const { icon, primary, to } = props;
+
+  const renderLink = React.useMemo(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    () => React.forwardRef<any, Omit<LinkProps, 'to'>>((itemProps, ref) => <Link to={to} ref={ref} {...itemProps} />),
+    [to],
+  );
+
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
+
 const Sidebar = (): React.ReactElement => {
   const networkId = useProviderStore((state) => state.networkId);
   const params = useParams<{ safeAddress: string }>();
+  const { pathname } = useLocation();
 
   return (
     <SidebarContainer>
       <SafeHeader network={upperFirst(ETHEREUM_NETWORK_TO_ID[networkId])} safeAddress={params.safeAddress} />
       <Line />
-      <a href="#test">Apps</a>
+      <List component="nav" aria-label="main safe features navigation">
+        <ListItemLink to={`${pathname}/apps`} primary="Apps" icon={<img src={ÅppsIcon} alt="Apps Icon" />} />
+      </List>
     </SidebarContainer>
   );
 };
