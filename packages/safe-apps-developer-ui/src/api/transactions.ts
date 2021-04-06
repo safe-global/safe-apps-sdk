eb391cf7593ae49c1ce2c6fc248fe80ba35a3a4f
Mikhail Mikheev <mmvsha73@gmail.com>
2021-04-06 14:04:32 +0400

    wip

--- packages/safe-apps-developer-ui/src/api/transactions.ts
+++ packages/safe-apps-developer-ui/src/api/transactions.ts
@@ -2,22 +2,28 @@ import MultiSendSol from '@gnosis.pm/safe-contracts/build/contracts/MultiSend.js
 import { Transaction } from '@gnosis.pm/safe-apps-sdk';
 import { ethers } from 'ethers';
 
-const encodeMultiSendCall = (signer: ethers.providers.JsonRpcSigner, txs: Transaction[]): string => {
-  const multiSend = new web3.eth.Contract(MultiSendSol.abi, MULTI_SEND_ADDRESS);
+const AbiCoder = new ethers.utils.AbiCoder();
+
+const encodeMultiSendCall = (
+  signer: ethers.providers.JsonRpcSigner,
+  multiSendAddress: string,
+  txs: Transaction[],
+): string => {
+  const multiSend = new ethers.Contract(multiSendAddress, MultiSendSol.abi, signer);
 
   const joinedTxs = txs
     .map((tx) =>
       [
-        web3.eth.abi.encodeParameter('uint8', 0).slice(-2),
-        web3.eth.abi.encodeParameter('address', tx.to).slice(-40),
-        web3.eth.abi.encodeParameter('uint256', tx.value).slice(-64),
-        web3.eth.abi.encodeParameter('uint256', web3.utils.hexToBytes(tx.data).length).slice(-64),
+        AbiCoder.encode(['uint8'], [0]).slice(-2),
+        AbiCoder.encode(['address'], [tx.to]).slice(-40),
+        AbiCoder.encode(['uint256'], [tx.value]).slice(-64),
+        AbiCoder.encode(['uint256'], [ethers.utils.arrayify(tx.data).length]).slice(-64),
         tx.data.replace(/^0x/, ''),
       ].join(''),
     )
     .join('');
 
-  const encodedMultiSendCallData = multiSend.methods.multiSend(`0x${joinedTxs}`).encodeABI();
+  const encodedMultiSendCallData = multiSend.encodeFunctionData('multiSend', [`0x${joinedTxs}`]);
 
   return encodedMultiSendCallData;
 };
