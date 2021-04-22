0e8d9761a633ddcd4cbb46743376a65b5adb75f8
Mikhail Mikheev <mmvsha73@gmail.com>
2021-04-22 13:35:23 +0400

    remove unused stuff

--- packages/safe-apps-developer-ui/src/utils/strings.ts
+++ packages/safe-apps-developer-ui/src/utils/strings.ts
@@ -1,28 +1,6 @@
-export const upperFirst = (value: string): string => value.charAt(0).toUpperCase() + value.toLowerCase().slice(1);
+const upperFirst = (value: string): string => value.charAt(0).toUpperCase() + value.toLowerCase().slice(1);
 
-export const capitalize = (value: string, prefix?: string): string => {
-  if (!value) {
-    return '';
-  }
-
-  if (typeof value === 'boolean') {
-    return prefix || '';
-  }
-
-  if (typeof value === 'number') {
-    return prefix ? `${prefix}${value}` : value;
-  }
-
-  if (typeof value !== 'string') {
-    return '';
-  }
-
-  const capitalized = upperFirst(value);
-
-  return prefix ? `${prefix}${capitalized}` : capitalized;
-};
-
-export const textShortener = (text: string, charsStart: number, charsEnd: number, separator = '...'): string => {
+const textShortener = (text: string, charsStart: number, charsEnd: number, separator = '...'): string => {
   const amountOfCharsToKeep = charsEnd + charsStart;
 
   if (amountOfCharsToKeep >= text.length || !amountOfCharsToKeep) {
@@ -42,3 +20,5 @@ export const textShortener = (text: string, charsStart: number, charsEnd: number
 
   return `${textStart}${separator}${textEnd}`;
 };
+
+export { upperFirst, textShortener };
