88bda61a05cdfe38028dad08856e46e6a0792cfe
Mikhail Mikheev <mmvsha73@gmail.com>
2021-04-26 15:34:36 +0400

    tests fix

--- packages/safe-apps-developer-ui/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome page', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Welcome to Gnosis Safe Multisig Developer Interface/i);
-  expect(linkElement).toBeInTheDocument();
-});
