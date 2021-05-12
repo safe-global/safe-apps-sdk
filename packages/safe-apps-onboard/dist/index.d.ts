4944c2d583b32e03fdb9c4fb0716e6ad3a7dd640
Mikhail Mikheev <mmvsha73@gmail.com>
2021-05-12 14:32:36 +0400

    add .eslintignore

---
+++ packages/safe-apps-onboard/dist/index.d.ts
@@ -0,0 +1,3 @@
+import { Initialization, API } from 'bnc-onboard/dist/src/interfaces';
+export declare const Onboard: (options: Initialization) => API;
+export default Onboard;
---
+++ packages/safe-apps-onboard/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Onboard = void 0;
+const wrapper_1 = require("./wrapper");
+const Onboard = (options) => {
+    return new wrapper_1.OnboardWrapper(options);
+};
+exports.Onboard = Onboard;
+exports.default = exports.Onboard;
+//# sourceMappingURL=index.js.map
