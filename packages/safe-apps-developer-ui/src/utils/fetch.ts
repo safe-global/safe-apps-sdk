d11424f4181894a5e4c72afbc53cbf6607e8f31a
Mikhail Mikheev <mmvsha73@gmail.com>
2021-05-14 14:02:51 +0400

    remove dev ui

--- packages/safe-apps-developer-ui/src/utils/fetch.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const fetchJSON = (url: RequestInfo, opts?: RequestInit): Promise<any> => fetch(url, opts).then((resp) => resp.json());
-
-export { fetchJSON };
