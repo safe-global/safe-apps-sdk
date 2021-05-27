8e29feaa8e41c2fb1e6ab72ba264bffc793c70d9
Mikhail Mikheev <mmvsha73@gmail.com>
2021-05-27 17:19:24 +0400

    Remove onboard wrapper

--- packages/safe-apps-onboard/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Initialization, API } from 'bnc-onboard/dist/src/interfaces';
-declare const Onboard: (options: Initialization) => API;
-export default Onboard;
--- packages/safe-apps-onboard/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { OnboardWrapper } from './wrapper';
-import { Initialization, API } from 'bnc-onboard/dist/src/interfaces';
-
-const Onboard = (options: Initialization): API => {
-  return new OnboardWrapper(options);
-};
-
-export default Onboard;
