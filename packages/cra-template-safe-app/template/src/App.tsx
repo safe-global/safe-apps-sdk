9f379cab1466741b1df033af3ee755f2ca94baa8
Mikhail Mikheev <mmvsha73@gmail.com>
2021-06-07 14:06:13 +0400

    use proxy hack for cors

--- packages/cra-template-safe-app/template/src/App.tsx
+++ packages/cra-template-safe-app/template/src/App.tsx
@@ -1,68 +1,43 @@
-import React, { useCallback, useState } from 'react';
-import styled from 'styled-components';
-import { Button, Loader, Title } from '@gnosis.pm/safe-react-components';
-import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
+import React, { useCallback } from "react"
+import styled from "styled-components"
+import { Button, Title } from "@gnosis.pm/safe-react-components"
+import { useSafeAppsSDK } from "@gnosis.pm/safe-apps-react-sdk"
 
-const Container = styled.form`
-  margin-bottom: 2rem;
-  width: 100%;
-  max-width: 480px;
+const Container = styled.div`
+  padding: 1rem;
+`
 
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-column-gap: 1rem;
-  grid-row-gap: 1rem;
-`;
-
-const App: React.FC = () => {
-  const { sdk, safe } = useSafeAppsSDK();
-  const [submitting, setSubmitting] = useState(false);
+const SafeApp = (): React.ReactElement => {
+  const { sdk, safe } = useSafeAppsSDK()
 
   const submitTx = useCallback(async () => {
-    setSubmitting(true);
     try {
       const { safeTxHash } = await sdk.txs.send({
         txs: [
           {
             to: safe.safeAddress,
-            value: '0',
-            data: '0x',
+            value: "0",
+            data: "0x",
           },
         ],
-      });
-      console.log({ safeTxHash });
-      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash);
-      console.log({ safeTx });
+      })
+      console.log({ safeTxHash })
+      const safeTx = await sdk.txs.getBySafeTxHash(safeTxHash)
+      console.log({ safeTx })
     } catch (e) {
-      console.error(e);
+      console.error(e)
     }
-    setSubmitting(false);
-  }, [safe, sdk]);
+  }, [safe, sdk])
 
   return (
     <Container>
-      <Title size="md">{safe.safeAddress}</Title>
-      {submitting ? (
-        <>
-          <Loader size="md" />
-          <br />
-          <Button
-            size="lg"
-            color="secondary"
-            onClick={() => {
-              setSubmitting(false);
-            }}
-          >
-            Cancel
-          </Button>
-        </>
-      ) : (
-        <Button size="lg" color="primary" onClick={submitTx}>
-          Submit
-        </Button>
-      )}
-    </Container>
-  );
-};
+      <Title size="md">Safe: {safe.safeAddress}</Title>
 
-export default App;
+      <Button size="lg" color="primary" onClick={submitTx}>
+        Click to send a test transaction
+      </Button>
+    </Container>
+  )
+}
+
+export default SafeApp
