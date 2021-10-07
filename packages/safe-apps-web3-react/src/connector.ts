8137ae6e5d04c1249566f311389c62f52ed0b84c
Mikhail Mikheev <mmvsha73@gmail.com>
2021-10-07 12:09:48 +0200

    Bug: safe app check takes 300ms when not in iframe (#226)
    * add checks to web3react/web3modal
    
    * add comments
    
    * add changeset
    
    Sent from my iPhone
    
    * fix iframe check
    
    Sent from my iPhone
    
    * build dist
    
    Sent from my iPhone

--- packages/safe-apps-web3-react/src/connector.ts
+++ packages/safe-apps-web3-react/src/connector.ts
@@ -47,6 +47,11 @@ class SafeAppConnector extends AbstractConnector {
   }
 
   public async isSafeApp(): Promise<boolean> {
+    // check if we're in an iframe
+    if (window?.parent === window) {
+      return false;
+    }
+
     const safe = await Promise.race([
       this.getSafeInfo(),
       new Promise<undefined>((resolve) => setTimeout(resolve, 300)),
