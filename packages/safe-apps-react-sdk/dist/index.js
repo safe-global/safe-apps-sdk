6d54d722873dbaa6fa51dbfff3d71d68f04668be
Mikhail <mmvsha73@gmail.com>
2022-06-21 15:50:42 +0200

    wagmi

--- packages/safe-apps-react-sdk/dist/index.js
+++ packages/safe-apps-react-sdk/dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -30,9 +21,9 @@ const SafeProvider = ({ loader = null, opts, children }) => {
     const contextValue = (0, react_1.useMemo)(() => ({ sdk, connected, safe }), [sdk, connected, safe]);
     (0, react_1.useEffect)(() => {
         let active = true;
-        const fetchSafeInfo = () => __awaiter(void 0, void 0, void 0, function* () {
+        const fetchSafeInfo = async () => {
             try {
-                const safeInfo = yield sdk.safe.getInfo();
+                const safeInfo = await sdk.safe.getInfo();
                 if (!active) {
                     return;
                 }
@@ -45,7 +36,7 @@ const SafeProvider = ({ loader = null, opts, children }) => {
                 }
                 setConnected(false);
             }
-        });
+        };
         fetchSafeInfo();
         return () => {
             active = false;
