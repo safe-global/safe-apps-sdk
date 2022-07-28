a772df8edb9dcecd1b4091b63807ec270b384918
Daniel Sanchez <daniel.sanchez@gnosis.pm>
2022-07-28 12:54:50 +0200

    Update dist folder (#349)

--- packages/safe-apps-sdk/dist/src/types/permissions.js
+++ packages/safe-apps-sdk/dist/src/types/permissions.js
@@ -7,6 +7,8 @@ class PermissionsError extends Error {
         super(message);
         this.code = code;
         this.data = data;
+        // Should adjust prototype manually because how TS handles the type extension compilation
+        // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
         Object.setPrototypeOf(this, PermissionsError.prototype);
     }
 }
