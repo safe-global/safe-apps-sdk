80fbd17e7fa12d93752943a0b689b04156bf5ce1
iamacook <aaron@safe.global>
2023-02-13 15:48:01 +0100

    chore: build

--- packages/safe-apps-sdk/dist/src/types/sdk.js
+++ packages/safe-apps-sdk/dist/src/types/sdk.js
@@ -1,8 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.isObjectEIP712TypedData = exports.NativeCurrency = void 0;
-var safe_gateway_typescript_sdk_1 = require("@safe-global/safe-gateway-typescript-sdk");
-Object.defineProperty(exports, "NativeCurrency", { enumerable: true, get: function () { return safe_gateway_typescript_sdk_1.NativeCurrency; } });
+exports.isObjectEIP712TypedData = void 0;
 const isObjectEIP712TypedData = (obj) => {
     return typeof obj === 'object' && obj != null && 'domain' in obj && 'types' in obj && 'message' in obj;
 };
