4508e6728d9c0322162f0c9bcb3b9d71514a0b1f
schmanu <manu@safe.global>
2023-05-04 14:26:01 +0200

    fix: types in sdk.d.ts

--- packages/safe-apps-sdk/dist/src/types/sdk.d.ts
+++ packages/safe-apps-sdk/dist/src/types/sdk.d.ts
@@ -1,73 +1,76 @@
 import { ChainInfo as _ChainInfo } from '@safe-global/safe-gateway-typescript-sdk';
 import { BigNumberish, BytesLike } from 'ethers';
-export type ChainInfo = Pick<_ChainInfo, 'chainName' | 'chainId' | 'shortName' | 'nativeCurrency' | 'blockExplorerUriTemplate'>;
+export type ChainInfo = Pick<
+  _ChainInfo,
+  'chainName' | 'chainId' | 'shortName' | 'nativeCurrency' | 'blockExplorerUriTemplate'
+>;
 export { NativeCurrency } from '@safe-global/safe-gateway-typescript-sdk';
 export type BaseTransaction = {
-    to: string;
-    value: string;
-    data: string;
+  to: string;
+  value: string;
+  data: string;
 };
 export type GetTxBySafeTxHashParams = {
-    safeTxHash: string;
+  safeTxHash: string;
 };
 export interface SendTransactionRequestParams {
-    safeTxGas?: number;
+  safeTxGas?: number;
 }
 export interface SendTransactionsParams {
-    txs: BaseTransaction[];
-    params?: SendTransactionRequestParams;
+  txs: BaseTransaction[];
+  params?: SendTransactionRequestParams;
 }
 export type GetBalanceParams = {
-    currency?: string;
+  currency?: string;
 };
 export type SignMessageParams = {
-    message: string;
+  message: string;
 };
 export interface TypedDataDomain {
-    name?: string;
-    version?: string;
-    chainId?: BigNumberish;
-    verifyingContract?: string;
-    salt?: BytesLike;
+  name?: string;
+  version?: string;
+  chainId?: BigNumberish;
+  verifyingContract?: string;
+  salt?: BytesLike;
 }
 export interface TypedDataTypes {
-    name: string;
-    type: string;
+  name: string;
+  type: string;
 }
 export type TypedMessageTypes = {
-    [key: string]: TypedDataTypes[];
+  [key: string]: TypedDataTypes[];
 };
 export type EIP712TypedData = {
-    domain: TypedDataDomain;
-    types: TypedMessageTypes;
-    message: Record<string, any>;
+  domain: TypedDataDomain;
+  types: TypedMessageTypes;
+  message: Record<string, any>;
 };
 export type SignTypedMessageParams = {
-    typedData: EIP712TypedData;
+  typedData: EIP712TypedData;
 };
 export type SendTransactionsResponse = {
-    safeTxHash: string;
+  safeTxHash: string;
 };
 export type OffChainSignMessageResponse = {
-    messageHash: string;
+  signature: string;
 };
 export type SignMessageResponse = SendTransactionsResponse | OffChainSignMessageResponse;
 export type SafeInfo = {
-    safeAddress: string;
-    chainId: number;
-    threshold: number;
-    owners: string[];
-    isReadOnly: boolean;
+  safeAddress: string;
+  chainId: number;
+  threshold: number;
+  owners: string[];
+  isReadOnly: boolean;
 };
 export type EnvironmentInfo = {
-    origin: string;
+  origin: string;
 };
 export type PostMessageOptions = {
-    transfer?: any[];
+  transfer?: any[];
 };
 export type AddressBookItem = {
-    address: string;
-    chainId: string;
-    name: string;
+  address: string;
+  chainId: string;
+  name: string;
 };
 export declare const isObjectEIP712TypedData: (obj?: unknown) => obj is EIP712TypedData;
