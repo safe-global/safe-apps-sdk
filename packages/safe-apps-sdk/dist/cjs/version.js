4d4d6b99b4c8a5808379f7f759146e8845e6351d
Daniel <25051234+dasanra@users.noreply.github.com>
2024-11-06 13:37:36 +0100

    chore(safe-apps-sdk): update wrong SDK version

--- packages/safe-apps-sdk/dist/cjs/version.js
+++ packages/safe-apps-sdk/dist/cjs/version.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getSDKVersion = void 0;
-const getSDKVersion = () => '9.0.0';
+const getSDKVersion = () => '9.1.0';
 exports.getSDKVersion = getSDKVersion;
 //# sourceMappingURL=version.js.map
--- packages/safe-apps-sdk/dist/esm/version.js
+++ packages/safe-apps-sdk/dist/esm/version.js
@@ -1,2 +1,2 @@
-export const getSDKVersion = () => '9.0.0';
+export const getSDKVersion = () => '9.1.0';
 //# sourceMappingURL=version.js.map
