b74c6102b24ea8e9416d8e9396f855a2d49c72e0
Aaron Cook <aaron@safe.global>
2025-03-26 08:03:44 +0000

    fix: update EIP-5792 implementation (#637)
    * fix: update EIP-5792 implementation
    
    * fix: move `chainId` check outside `map`
    
    * fix: build

--- packages/safe-apps-provider/dist/types.d.ts
+++ packages/safe-apps-provider/dist/types.d.ts
@@ -23,3 +23,45 @@ export interface EIP1193Provider extends EventEmitter {
     disconnect(): Promise<void>;
     request(args: RequestArguments): Promise<unknown>;
 }
+type Capability = {
+    [key: string]: unknown;
+    optional?: boolean;
+};
+export type SendCallsParams = {
+    version: string;
+    id?: string;
+    from?: `0x${string}`;
+    chainId: `0x${string}`;
+    calls: Array<{
+        to?: `0x${string}`;
+        data?: `0x${string}`;
+        value?: `0x${string}`;
+        capabilities?: Record<string, Capability>;
+    }>;
+    capabilities?: Record<string, Capability>;
+};
+export type SendCallsResult = {
+    id: string;
+    capabilities?: Record<string, unknown>;
+};
+export type GetCallsParams = `0x${string}`;
+export type GetCallsResult = {
+    version: '1.0';
+    id: `0x${string}`;
+    chainId: `0x${string}`;
+    status: 100 | 200 | 400 | 500 | 600;
+    receipts?: Array<{
+        logs: Array<{
+            address: `0x${string}`;
+            data: `0x${string}`;
+            topics: Array<`0x${string}`>;
+        }>;
+        status: `0x${string}`;
+        blockHash: `0x${string}`;
+        blockNumber: `0x${string}`;
+        gasUsed: `0x${string}`;
+        transactionHash: `0x${string}`;
+    }>;
+    capabilities?: Record<string, unknown>;
+};
+export {};
